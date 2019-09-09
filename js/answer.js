7852857637e62d1ca2a5030a6546fd476c60a472
hrkt <hiroki110@gmail.com>
2019-09-10 05:51:02 +0900

    add Number example

--- js/answer.js
+++ js/answer.js
@@ -5,3 +5,11 @@ exports.answer42 = function() {
     const z = 12602123297335631n
     return  x ** 3n + y ** 3n + z ** 3n
 }
+
+// using BigInt(https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/BigInt)
+exports.answer42InNumeric = function() {
+    const x = -80538738812075974
+    const y = 80435758145817515
+    const z = 12602123297335631
+    return  x ** 3 + y ** 3 + z ** 3
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 const answer = require('./answer')
-console.log(answer.answer42())
+console.log("BigInt:" + answer.answer42())
+console.log("Numeric:" + answer.answer42InNumeric())
